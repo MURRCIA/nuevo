fc181eb66dec3c619ccb2adba78ef10f2b34ef87
murrcia <mzjesusr@gmail.com>
2025-03-02 17:27:54 -0500

    Set up Express server with authentication routes and middleware

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import middleware from '../middleware/auth.js';
+
+const router = express.Router();
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+
+// ruta protegidas 
+router.get('/me', middleware, me);
+
+
+
+
+
+//www.casa.com/api/auth/register // ruta para registrar un usuario
+//www.casa.com/api/auth/register // post 
+//www.casa.com/user=samuel$password=1234 //get 
--- server.js
+++ server.js
@@ -1,1 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import middleware from './middleware/auth.js';
+import authRoutes from './routes/auth.js';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);// auth routes el usuario ingresa a api/auth/register
+app.get("/api/protected", middleware, (req, res) => {
+    res.json({
+        message: 'Protected route'
+    });
+});
+
+app.use((req, res, next) => {
+    res.status(500).json({
+        message: 'Internal server error'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
 
