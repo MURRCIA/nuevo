69614ff0a1c6764905c6c2fd5da5886721d39155
murrcia <mzjesusr@gmail.com>
2025-03-02 21:53:38 -0500

    Configure dynamic port for server deployment

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
         message: 'Internal server error'
     });
 });
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 
